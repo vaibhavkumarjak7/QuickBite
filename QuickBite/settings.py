1f6c9e27b33e165bc4b6356bf6bc43f9acecab89
Vaibhav <vaibhavkumarjak@gmail.com>
2026-06-07 19:15:54 +0530

    Add throttling limit to API

--- QuickBite/settings.py
+++ QuickBite/settings.py
@@ -156,6 +156,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'pictures')
 MEDIA_URL = '/pictures/'
 
 REST_FRAMEWORK = {
+    "DEFAULT_THROTTLE_CLASSES":[
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES":{
+      "anon":"10/minute",
+      "user":"1000/day",  
+    },
     "DEFAULT_PAGINATION_CLASS" : "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE" : 5,
 
